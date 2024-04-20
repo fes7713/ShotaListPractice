1ca79e6c39ad4a395c82c758255e40e34d7d09bd
SHOTAiiMURA <hn.j.shota027503@gamil.com>
2024-04-20 12:50:29 +0900

    creating signle lincked list

--- ArrayList.py
+++ ArrayList.py
@@ -10,25 +10,21 @@ class ArrayList(List):
     # Add data to array
     def add(self, data):
         self.array.append(data)
+
         return
 
     # Insert data to the index
     # Make sure to shift datas
     def insert(self, index, data):
-        # if index < 0 or index > self.size:
-        #     print("Error size")
+
         self.array.insert(index, data)
-        # else:
-        #     self.array.insert(index, data)
-        #     self.size +=1
-        save_value = self.array[-1]
 
         # 配列の後ろから2番目の要素から順に後ろの要素に値をコピー
-        for i in range(len(self.array) - 2, -1, -1):
+        #for i in range(length,where to start, where to end, how many step)
+        for i in range(len(self.array) - 2, 1, -1):
             self.array[i]
 
-        # 一時的に保存した値を配列の先頭に挿入
-        self.array[-1] = save_value
+
 
     # Search for data in array and remove data
     # Make sure to shift all the gaps
@@ -45,7 +41,7 @@ class ArrayList(List):
     # Return current size
     def size(self)-> int:
 
-        return len(self.size)
+        return len(int(self.size))
 
     # Return array that represents the list
     def toArray(self):
--- SinglyLinkedList.py
+++ SinglyLinkedList.py
@@ -1,23 +1,33 @@
 from List import List
+from Node import Node
 
 
 class SinglyLinkedList(List):
-    def __int__(self):
-        super.__init__()
+    def __init__(self):
         self.head = None
         self.size = 0
         return
 
-    # Create new node
+    # Create new node: done
     # Search for the last node
     # Change the next node of the last node to the created node
     def add(self, data):
-
+        #cretae new node
+        self.new_node = Node(data)
+        #if head != none: which means not empty, new_node become as head
+        if self.head is None:
+            self.head = self.new_node
+        # Search for the last node
+        #last_node = self.head
+        # Change the next node of the last node to the created node
+       # last_node = next(self.head)
         return
 
     # Insert data to the index
     # Create new node and insert to the middle of the nodes
     def insert(self, index, data):
+        if self.head is None:
+            self.head = Node(index, data, None)
         return
 
     # Search for data in array and remove node
@@ -27,15 +37,18 @@ class SinglyLinkedList(List):
     # Return true when size is zero
     # Otherwise false
     def isEmpty(self)-> bool:
+        if self.head == None:
+            return True
+
         return False
 
     # Return current size
     def size(self)-> int:
-        return 0
+        return len(int(self.size))
 
     # Return array that represents the list
     def toArray(self):
-        return []
+        return self.head
 
     # Print inside of array
     # Call toArray inside this function to print it
